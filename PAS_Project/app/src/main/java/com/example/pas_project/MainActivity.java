e9ef7963feb38228f556019fb9f61c90424af992
DylFialho <21135@stu.ipbeja.pt>
2021-11-16 17:08:32 +0000

    bottombar finish

--- PAS_Project/app/src/main/java/com/example/pas_project/MainActivity.java
+++ PAS_Project/app/src/main/java/com/example/pas_project/MainActivity.java
@@ -3,17 +3,46 @@ package com.example.pas_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomView customView;
+
+    private final int ID_Home = 1;
+    private final int ID_News = 2;
+    private final int ID_Cart = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        customView = findViewById(R.id.customView);
 
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottomNav);
+
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_Home, R.mipmap.game_controller));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_Cart, R.drawable.ic_baseline_shopping_cart_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_News, R.drawable.ic_baseline_news_24));
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+
+            }
+        });
+
+
+        bottomNavigation.setCount(ID_Cart, "2");
+        bottomNavigation.show(ID_Home, true);
     }
 }
