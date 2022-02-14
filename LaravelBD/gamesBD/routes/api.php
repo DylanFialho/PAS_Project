76daafca2bba9a17e3e8a830868a4ad927271d41
DylanFialho <21135@stu.ipbeja.pt>
2022-02-14 17:54:51 +0000

    Update api.php

--- LaravelBD/gamesBD/routes/api.php
+++ LaravelBD/gamesBD/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\UserApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,9 @@ Route::get('games/{id}', [ApiController::class, 'show']);
 Route::post('games', [ApiController::class, 'create']);
 Route::put('games/{id}', [ApiController::class, 'update']);
 Route::delete('games/{id}', [ApiController::class, 'destroy']);
+
+Route::get('user', [UserApiController::class, 'index']);
+Route::get('user/{id}', [UserApiController::class, 'show']);
+Route::post('user', [UserApiController::class, 'create']);
+Route::put('user/{id}', [UserApiController::class, 'update']);
+Route::delete('user/{id}', [UserApiController::class, 'destroy']);
